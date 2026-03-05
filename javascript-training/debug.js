536b99828c9dcf45e5d04b9945155dc85aa7fa5f
sanjaykjodatti-glitch <sanjaykjodatti@gmail.com>
2026-03-05 13:32:30 +0530

    Assignments

---
+++ javascript-training/debug.js
@@ -0,0 +1,13 @@
+console.log("line 1");
+console.log("line 2");
+console.log("line 3");
+console.log("line 4");
+sumofNumbers (1,2);
+console.log("line 5");
+
+//sum of numbers login
+function sumofNumbers(a,b)
+{
+    c=(a+b);
+    console.log("sum is:"+c);
+}
