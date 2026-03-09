06a9dc53d2c251e68ba2ccac4dc0b1b2e1739704
sanjaykjodatti-glitch <sanjaykjodatti@gmail.com>
2026-03-09 23:04:51 +0530

    Assignments1

---
+++ typescript-training/Assignments/Assignments1.ts
@@ -0,0 +1,91 @@
+//1.City temprature
+let cityTemperature:number= 25.5;
+console.log("City Temperature:",cityTemperature, "°C","data type:",typeof cityTemperature);
+
+//2.Whether a customer has placed an order: true or false
+let hasCustomerPlacedOrder:boolean=false;
+console.log("hasCustomerPlacedOrder:",hasCustomerPlacedOrder,"data type:",typeof hasCustomerPlacedOrder);
+
+//3.Person's phone number
+let phoneNumber:string="123-456-7890"
+console.log("phonenumber:", phoneNumber,"data type:",typeof phoneNumber);
+
+//4. Amount of money in a customer's bank account:
+let money:number=1000.50;
+console.log("Amount of money in a customer's bank account is:", 1000.50,"rupees:","data type:",typeof money);
+
+//5. Person's email address:
+let emailAddress:string="john.doe@example.com";
+console.log("email id:", emailAddress,"data type:",typeof emailAddress);
+
+//6.Coordinates of a location
+let coordinates: number[] = [37.7749, -122.4194];
+console.log(coordinates, typeof coordinates );
+
+//7. Person's marital status: true or false
+let isPersonMarried:boolean=true;
+console.log("isPersonMarried:", isPersonMarried);
+
+//8. Person's occupation:
+let personOccupation:String="Software Engineer";
+console.log("person occupation is",personOccupation);
+
+//9. Person's favourite colour:
+let personfavColor:String="Blue";
+console.log("personfavColor is",personfavColor);
+
+//10.Current year
+let currentYear:number=2023;
+console.log("currentYear is",currentYear);
+
+//11.11.Number of followers on a social media platform
+let followers: number = 1000000;
+console.log(followers);
+console.log(typeof followers);
+
+//12.Rating of a movie:
+let movieRating: number = 7.5;
+console.log(movieRating);
+console.log(typeof movieRating);
+
+//13.Person's blood type
+let bloodType: string = 'A';
+console.log(bloodType);
+console.log(typeof bloodType);
+
+//14.14.Title of a book
+let bookTitle: string = "To Kill a Mockingbird";
+console.log(bookTitle);
+console.log(typeof bookTitle);
+
+//15.Number of employees in a company
+let numberOfEmployees: number = 500;
+console.log(numberOfEmployees);
+console.log(typeof numberOfEmployees);
+
+//16.Time of an event
+let eventTime: string = "2:30 PM";
+console.log(eventTime);
+console.log(typeof eventTime);
+
+//17.Name of a country:
+let countryName: string = "United States";
+console.log(countryName);
+console.log(typeof countryName);
+
+//18.Person's eye color
+let eyeColor: string = "Brown";
+console.log(eyeColor);
+console.log(typeof eyeColor);
+
+//19.Person's birthplace
+let birthplace: string = "New York City";
+
+console.log(birthplace);
+console.log(typeof birthplace);
+
+//20. Distance between two cities
+let distance: number = 200.5;
+console.log(distance);
+console.log(typeof distance);
+
